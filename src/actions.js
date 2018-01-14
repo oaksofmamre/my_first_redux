f61fe5ef26a79e2a351834411637f77d173fee98
DC <heyyodc-git@yahoo.com>
2018-01-13 18:56:04 -0800

    my first redux app

---
+++ src/actions.js
@@ -0,0 +1,40 @@
+// Set constants for action types
+export const CREATE_PUPPY = 'CREATE_PUPPY'
+export const ADOPT_PUPPY = 'ADOPT_PUPPY'
+export const UPDATE_PUPPY = 'UPDATE_PUPPY'
+export const SET_AVAILABILITY_FILTER = 'SET_AVAILABILITY_FILTER'
+
+// Action creators for each action
+
+// Set up an incrementing id for each puppy
+let nextPuppyId = 1
+export function createPuppy(data) {
+  return {
+    type: CREATE_PUPPY,
+    data: {
+      ...data,
+      id: nextPuppyId++
+    }
+  }
+}
+
+export function adoptPuppy(id) {
+  return {
+    type: ADOPT_PUPPY,
+    data: id,
+  }
+}
+
+export function updatePuppy(data) {
+  return {
+    type: UPDATE_PUPPY,
+    data,
+  }
+}
+
+export function setAvailabilityFilter(data) {
+  return {
+    type: SET_AVAILABILITY_FILTER,
+    data,
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Import the createStore method from Redux
+import {createStore} from 'redux'
+// Import our reducer to use when we create the store
+import {puppiesApp} from './reducers'
+// Import our action creators for when we dispatch
+import {
+  createPuppy,
+  adoptPuppy,
+  updatePuppy,
+  setAvailabilityFilter,
+} from './actions'
+
+// Create the store, passing in the reducer as the
+// only argument
+let store = createStore(puppiesApp)
+
+// Set up the listener. The subscribe method returns a
+// function to unregister the listener so set it equal
+// to a variable for later use.
+let unsubscribe = store.subscribe(() => {
+  // Log the new state to the console
+  console.log(store.getState())
+})
+
+// Before we start, take a look at the state
+console.log('initial state', store.getState())
+
+// Dispatch our actions
+store.dispatch(createPuppy({
+  name: 'Sean Connery',
+  breed: 'Scottish Terrier',
+  available: true,
+}))
+
+store.dispatch(createPuppy({
+  name: 'Priscilla Queen of the Desert',
+  breed: 'Australian Sheperd',
+  available: true,
+}))
+
+store.dispatch(createPuppy({
+  name: 'Prince Harry',
+  breed: 'Jack Russell Terrier',
+  available: true,
+}))
+
+store.dispatch(adoptPuppy(3))
+
+store.dispatch(updatePuppy({
+  id: 3,
+  name: 'Prince Harold',
+  breed: 'Jack Russell Terrier',
+  available: true,
+}))
+
+store.dispatch(setAvailabilityFilter('SHOW_ADOPTED'))
+
+// Unregister/cancel the listener
+unsubscribe()
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
